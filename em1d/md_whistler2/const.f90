07a089c76ac78d0ee24f2a305e1073853aa36dcf
Yosuke Matsumoto <ymatumot@chiba-u.jp>
2011-07-05 09:55:28 +0900

    2次元衝撃波の課題が追加されていなかったので、追加。1次元3成分whistler励起の課題は削除。

--- em1d/md_whistler2/const.f90
+++
@@ -1,9 +0,0 @@
-module const
-
-  implicit none
-  integer, parameter :: nx  = 100    ! number of grid points
-  integer, parameter :: np  = 2000    ! number of particles in each cell
-  integer, parameter :: nsp = 3     ! number of particle species
-  integer, parameter :: bc  = 0     ! boundary condition (periodic:0, reflective:-1)
-end module
-  
